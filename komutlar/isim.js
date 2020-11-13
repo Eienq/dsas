c1a078dfc66c54f7e279993bfbded8aaff439d95
Glitch (zest-furry-cobalt) <none>
2020-11-13 10:30:48 +0000

    🏏😎 Checkpoint ./komutlar/isim.js:14888641/1506 ./komutlar/yapım.js:14888641/3228

--- komutlar/isim.js
+++ komutlar/isim.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js");
-const Gamedig = require('gamedig');
 exports.run = async (client, message, args) => {
+  
+  
  
     const embed = new Discord.MessageEmbed()
    .setColor('BLUE')
-    .setImage(`https://cache.gametracker.com/server_info/185.193.165.235:27015/b_560_95_1.png`) 
+    .setDescription(`https://www.gametracker.com/server_info/185.193.165.235:27015/`) 
     message.channel.send(embed)
 
   
